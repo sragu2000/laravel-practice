f62de1b85782cdcc0d5924447c84c6ea60883aa1
sragu2000 <ragurajsivanantham@gmail.com>
2022-09-18 17:42:43 +0530

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\First;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Without Controller
+// Route::get("addNumbers",function(Request $r){
+//     $firstNumber = intval($r->fn);
+//     $secondNumber = intval($r->sn);
+//     $total=$firstNumber+$secondNumber;
+//     return response()->json(array("total"=>$total),200);
+// });
+
+//With Controller 
+Route::get("addNumbers",[First::class,"addNumbers"]);
